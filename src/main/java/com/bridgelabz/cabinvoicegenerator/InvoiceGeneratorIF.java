7d85cceb581bce016e87a577f7f3a58388d2bf5a
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-23 04:24:03 +0530

    Added: To initially implement calculate rate function

---
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorIF.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+public interface InvoiceGeneratorIF {
+
+	public Double calculateFare(Double distance, Integer time);
+}
---
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorImpl.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+public class InvoiceGeneratorImpl implements InvoiceGeneratorIF {
+
+	@Override
+	public Double calculateFare(Double distance, Integer time) {
+		Double fare = 0.0;
+		return fare;
+	}
+}
