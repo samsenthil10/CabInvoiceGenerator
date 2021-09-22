a54dcd3f6f54c26e2f1eb32ea917859b19591441
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-23 04:46:17 +0530

    Modified: To calculate fare based on given test case

--- src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorImpl.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorImpl.java
@@ -4,7 +4,11 @@ public class InvoiceGeneratorImpl implements InvoiceGeneratorIF {
 
 	@Override
 	public Double calculateFare(Double distance, Integer time) {
-		Double fare = 0.0;
+
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator(); 
+		Double fare = (invoiceGenerator.RATE_PER_KILOMETER*distance)+(invoiceGenerator.RATE_PER_MINUTE*time);
+		if(distance > 0.0 && time > 0 && fare < 5.0 && fare > 0.0)
+			return 5.0;
 		return fare;
 	}
 }
