c52265a24a9c15c5699d5ff22cc2fd14a7dfab2d
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-23 04:44:18 +0530

    Added: To throw exceptions when record is invalid

--- src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
@@ -3,6 +3,8 @@ package com.bridgelabz.cabinvoicegenerator;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.bridgelabz.cabinvoicegenerator.InvoiceGeneratorException.exceptionType;
+
 public class InvoiceServiceTest {
 
 	@Test
@@ -10,9 +12,31 @@ public class InvoiceServiceTest {
 		
 		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		InvoiceGeneratorIF invoiceGeneratorOperations = new InvoiceGeneratorImpl();	
-		invoiceGenerator.setDistance(2.0);
-		invoiceGenerator.setTime(1);
+		Double distance = 2.0;
+		Integer time = 5;
+		createRecord(invoiceGenerator, distance, time);
 		Double fare = invoiceGeneratorOperations.calculateFare(invoiceGenerator.getDistance(), invoiceGenerator.getTime());
 		Assert.assertEquals(fare,(Double.valueOf(25.0)),0.0);
+	}
+
+	private void createRecord(InvoiceGenerator invoiceGenerator, Double distance, Integer time) {
+		
+		try {
+			if(distance<=0)
+				throw new InvoiceGeneratorException(exceptionType.DISTANCE_INVALID,"Invalid Distance!");
+			invoiceGenerator.setDistance(distance);
+		}
+		catch(NullPointerException e) {
+			throw new InvoiceGeneratorException(exceptionType.DISTANCE_NULL,"Null Distance!");
+		}
+		
+		try {
+			if(time<0)
+				throw new InvoiceGeneratorException(exceptionType.TIME_INVALID,"Invalid Time!");
+			invoiceGenerator.setTime(time);
+		}
+		catch(NullPointerException e) {
+			throw new InvoiceGeneratorException(exceptionType.TIME_NULL,"Null Time!");
+		}
 	}
 }
