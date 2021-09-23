a109e1f350e82acd376b9696d4ef1eca05ec67bb
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-23 05:34:31 +0530

    Added: Test case to validate aggregate total of all rides

--- src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
@@ -1,5 +1,8 @@
 package com.bridgelabz.cabinvoicegenerator;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,43 +12,60 @@ public class InvoiceServiceTest {
 
 	@Test
 	public void givenDistanceAndTime_ShouldReturnTotalFare() {
-		
-		Ride invoiceGenerator = new Ride();
-		InvoiceGeneratorIF invoiceGeneratorOperations = new InvoiceGeneratorImpl();	
+
+		Ride ride = new Ride();
+		InvoiceGeneratorIF rideOperations = new InvoiceGeneratorImpl();	
 		Double distance = 2.0;
 		Integer time = 5;
-		createRecord(invoiceGenerator, distance, time);
-		Double fare = invoiceGeneratorOperations.calculateFare(invoiceGenerator.getDistance(), invoiceGenerator.getTime());
+		createRecord(ride, distance, time);
+		Double fare = rideOperations.calculateFare(ride);
 		Assert.assertEquals(fare,(Double.valueOf(25.0)),0.0);
 	}
-	
+
 	@Test
 	public void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
-		
-		Ride invoiceGenerator = new Ride();
-		InvoiceGeneratorIF invoiceGeneratorOperations = new InvoiceGeneratorImpl();	
+
+		Ride ride = new Ride();
+		InvoiceGeneratorIF rideOperations = new InvoiceGeneratorImpl();	
 		Double distance = 0.1;
 		Integer time = 1;
-		createRecord(invoiceGenerator, distance, time);
-		Double fare = invoiceGeneratorOperations.calculateFare(invoiceGenerator.getDistance(), invoiceGenerator.getTime());
+		createRecord(ride, distance, time);
+		Double fare = rideOperations.calculateFare(ride);
 		Assert.assertEquals(fare,(Double.valueOf(5.0)),0.0);
 	}
 
-	private void createRecord(Ride invoiceGenerator, Double distance, Integer time) {
-		
+	@Test
+	public void givenMultipleRide_ShouldReturnAggregateTotalFareForAllRides() {
+
+		List<Ride> rides = new LinkedList<Ride>();
+		InvoiceGeneratorIF rideOperations = new InvoiceGeneratorImpl();	
+		Double distance[] = {0.1,2.0};
+		Integer time[] = {1,5};
+		for(int index = 0; index < distance.length; index++) {
+
+			Ride ride = new Ride();
+			createRecord(ride, distance[index], time[index]);
+			rides.add(ride);
+		}
+		Double totalFare = rideOperations.calculateFare(rides);
+		Assert.assertEquals(totalFare,(Double.valueOf(30.0)),0.0);
+	}
+
+	private void createRecord(Ride ride, Double distance, Integer time) {
+
 		try {
 			if(distance<=0)
 				throw new InvoiceGeneratorException(exceptionType.DISTANCE_INVALID,"Invalid Distance!");
-			invoiceGenerator.setDistance(distance);
+			ride.setDistance(distance);
 		}
 		catch(NullPointerException e) {
 			throw new InvoiceGeneratorException(exceptionType.DISTANCE_NULL,"Null Distance!");
 		}
-		
+
 		try {
 			if(time<0)
 				throw new InvoiceGeneratorException(exceptionType.TIME_INVALID,"Invalid Time!");
-			invoiceGenerator.setTime(time);
+			ride.setTime(time);
 		}
 		catch(NullPointerException e) {
 			throw new InvoiceGeneratorException(exceptionType.TIME_NULL,"Null Time!");
