9344cd7fc6c410981a7225b53f5287ee5f251f23
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-23 05:51:37 +0530

    Added: To generate invoice summary

--- src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorIF.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorIF.java
@@ -6,5 +6,6 @@ public interface InvoiceGeneratorIF {
 
 	public Double calculateFare(Ride ride);
 	public Double calculateFare(List<Ride> rides);
+	public InvoiceSummary calculateSummary(List<Ride> rides);
 	
 }
--- src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorImpl.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorImpl.java
@@ -8,9 +8,7 @@ public class InvoiceGeneratorImpl implements InvoiceGeneratorIF {
 	public Double calculateFare(Ride ride) {
 
 		Double fare = (ride.MINIMUM_RATE_PER_KILOMETER*ride.getDistance())+(ride.MINIMUM_RATE_PER_MINUTE*ride.getTime());
-		if(ride.getDistance() > 0.0 && ride.getTime() > 0 && fare < 5.0 && fare > 0.0)
-			return 5.0;
-		return fare;
+		return Math.max(fare, 5.0);
 	}
 
 	@Override
@@ -22,5 +20,21 @@ public class InvoiceGeneratorImpl implements InvoiceGeneratorIF {
 			totalFare+=total;
 		}
 		return totalFare;
+	}
+
+	@Override
+	public InvoiceSummary calculateSummary(List<Ride> rides) {
+
+		InvoiceSummary invoiceSummary = new InvoiceSummary();
+		Integer totalRides = rides.size();
+		invoiceSummary.setTotalNumberOfRides(totalRides);
+		Double totalFare = 0.0;
+		for (Ride ride : rides) {
+			Double total = calculateFare(ride);
+			totalFare+=total;
+		}
+		invoiceSummary.setTotalFare(totalFare);
+		invoiceSummary.setAverageFare(totalFare/totalRides);
+		return invoiceSummary;
 	}
 }
